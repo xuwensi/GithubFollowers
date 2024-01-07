5875ce9ef8c1fe111baa147aee180c6605bf0616
Wensi Xu <xuwensi0515@gmail.com>
2024-01-06 16:44:50 -0800

    Adds starter code

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Wensi Xu on 1/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
